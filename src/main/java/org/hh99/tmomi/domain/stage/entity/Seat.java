3899b64eca548669574e57c12e9b97ab46197f34
heroq <96118954+heroq@users.noreply.github.com>
2024-01-31 20:13:00 +0900

    Refactor: 지연로딩으로 변경

--- src/main/java/org/hh99/tmomi/domain/stage/entity/Seat.java
+++ src/main/java/org/hh99/tmomi/domain/stage/entity/Seat.java
@@ -4,6 +4,7 @@ import org.hh99.tmomi.domain.stage.dto.seat.SeatRequestDto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,7 +23,7 @@ public class Seat {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "stage_id")
 	private Stage stage;
 
