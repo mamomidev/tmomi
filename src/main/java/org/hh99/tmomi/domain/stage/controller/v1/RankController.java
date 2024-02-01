34dd577094bc600c44eded83e0c124278262610b
heroq <96118954+heroq@users.noreply.github.com>
2024-02-01 19:50:27 +0900

    Feat: 등급 컨트롤러 기능 추가

--- src/main/java/org/hh99/tmomi/domain/stage/controller/v1/RankController.java
+++ src/main/java/org/hh99/tmomi/domain/stage/controller/v1/RankController.java
@@ -1,9 +1,18 @@
 package org.hh99.tmomi.domain.stage.controller.v1;
 
+import java.util.List;
+
+import org.hh99.tmomi.domain.stage.dto.rank.RankRequestDto;
+import org.hh99.tmomi.domain.stage.dto.rank.RankResponseDto;
+import org.hh99.tmomi.domain.stage.service.RankService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +20,30 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1/admin")
 public class RankController {
 
-	@PostMapping("/v1/rank")
-	public void createRank() {
+	private final RankService rankService;
 
+	@GetMapping("/stage/{stageId}/seat")
+	public ResponseEntity<List<RankResponseDto>> getRankListByStageId(@PathVariable Long stageId) {
+		return ResponseEntity.ok(rankService.getRankListByStageId(stageId));
 	}
 
-	@PutMapping("/v1/rank/{rankId}")
-	public void updateRank(@PathVariable Long rankId) {
-
+	@PostMapping("/rank")
+	public ResponseEntity<RankResponseDto> createRank(@RequestBody RankRequestDto rankRequestDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(rankService.createRank(rankRequestDto));
 	}
 
-	@DeleteMapping("/v1/rank/{rankId}")
-	public void deleteRank(@PathVariable Long rankId) {
+	@PutMapping("/rank/{rankId}")
+	public ResponseEntity updateRank(@PathVariable Long rankId, @RequestBody RankRequestDto rankRequestDto) {
+		rankService.updateRank(rankId, rankRequestDto);
+		return ResponseEntity.ok().build();
+	}
 
+	@DeleteMapping("/rank/{rankId}")
+	public ResponseEntity deleteRank(@PathVariable Long rankId) {
+		rankService.deleteRank(rankId);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
