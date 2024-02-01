c40a71a246501c52272d7f585fd8d46e394453f6
heroq <96118954+heroq@users.noreply.github.com>
2024-02-01 19:43:23 +0900

    Feat: 좌석 컨트롤러 기능 추가

--- src/main/java/org/hh99/tmomi/domain/stage/controller/v1/SeatController.java
+++ src/main/java/org/hh99/tmomi/domain/stage/controller/v1/SeatController.java
@@ -1,9 +1,18 @@
 package org.hh99.tmomi.domain.stage.controller.v1;
 
+import java.util.List;
+
+import org.hh99.tmomi.domain.stage.dto.seat.SeatRequestDto;
+import org.hh99.tmomi.domain.stage.dto.seat.SeatResponseDto;
+import org.hh99.tmomi.domain.stage.service.SeatService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,21 +20,30 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1/admin")
 public class SeatController {
 
-	@PostMapping("/v1/seat")
-	public void createSeat() {
+	private final SeatService seatService;
 
+	@GetMapping("/stage/{stageId}/seat")
+	public ResponseEntity<List<SeatResponseDto>> getSeatListByStageId(@PathVariable Long stageId) {
+		return ResponseEntity.ok(seatService.getSeatListByStageId(stageId));
 	}
 
-	@PutMapping("/v1/seat/{seatId}")
-	public void updateSeat(@PathVariable Long seatId) {
-
+	@PostMapping("/seat")
+	public ResponseEntity<SeatResponseDto> createSeat(@RequestBody SeatRequestDto seatRequestDto) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(seatService.createSeat(seatRequestDto));
 	}
 
-	@DeleteMapping("/v1/seat/{seatId}")
-	public void deleteSeat(@PathVariable Long seatId) {
+	@PutMapping("/seat/{seatId}")
+	public ResponseEntity updateSeat(@PathVariable Long seatId, @RequestBody SeatRequestDto seatRequestDto) {
+		seatService.updateSeat(seatId, seatRequestDto);
+		return ResponseEntity.ok().build();
+	}
 
+	@DeleteMapping("/seat/{seatId}")
+	public ResponseEntity deleteSeat(@PathVariable Long seatId) {
+		seatService.deleteSeat(seatId);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
