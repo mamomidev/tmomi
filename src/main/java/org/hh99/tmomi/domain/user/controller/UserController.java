f6e3ff6edaffd1802d19cf81483ab52eab09cfb9
heroq <96118954+heroq@users.noreply.github.com>
2024-02-03 00:12:04 +0900

    Refactor: 쿠키 컨트롤러에서 넣게 수정

--- src/main/java/org/hh99/tmomi/domain/user/controller/UserController.java
+++ src/main/java/org/hh99/tmomi/domain/user/controller/UserController.java
@@ -1,16 +1,19 @@
 package org.hh99.tmomi.domain.user.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 import org.hh99.tmomi.domain.user.dto.UserRequestDto;
 import org.hh99.tmomi.domain.user.dto.UserResponseDto;
 import org.hh99.tmomi.domain.user.service.UserService;
+import org.hh99.tmomi.global.jwt.JwtToken;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -29,7 +32,13 @@ public class UserController {
 	@PostMapping("/signin")
 	public ResponseEntity signIn(HttpServletResponse httpServletResponse,
 		@RequestBody UserRequestDto userRequestDto) throws UnsupportedEncodingException {
-		userService.signIn(userRequestDto, httpServletResponse);
+		JwtToken jwtToken = userService.signIn(userRequestDto);
+		Cookie cookie = new Cookie("Authorization",
+			URLEncoder.encode("Bearer " + jwtToken.getAccessToken(), "utf-8").replaceAll("\\+", "%20"));
+		cookie.setPath("/");
+		cookie.setMaxAge(60 * 60);  // 쿠키 유효 시간 : 1시간
+		httpServletResponse.addCookie(cookie);
+
 		return ResponseEntity.ok().build();
 	}
 }
--- src/main/java/org/hh99/tmomi/domain/user/service/UserService.java
+++ src/main/java/org/hh99/tmomi/domain/user/service/UserService.java
@@ -1,8 +1,5 @@
 package org.hh99.tmomi.domain.user.service;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
 import org.hh99.tmomi.domain.user.UserAuthEnum;
 import org.hh99.tmomi.domain.user.dto.UserRequestDto;
 import org.hh99.tmomi.domain.user.dto.UserResponseDto;
@@ -20,8 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -35,21 +30,12 @@ public class UserService implements UserDetailsService {
 	private final PasswordEncoder passwordEncoder;
 
 	@Transactional
-	public JwtToken signIn(UserRequestDto userRequestDto, HttpServletResponse httpServletResponse) throws
-		UnsupportedEncodingException {
+	public JwtToken signIn(UserRequestDto userRequestDto) {
 		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
 			userRequestDto.getEmail(),
 			userRequestDto.getPassword());
 		Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-
-		JwtToken jwtToken = jwtTokenProvider.generateToken(authentication);
-		Cookie cookie = new Cookie("Authorization",
-			URLEncoder.encode("Bearer " + jwtToken.getAccessToken(), "utf-8").replaceAll("\\+", "%20"));
-		cookie.setPath("/");
-		cookie.setMaxAge(60 * 60);  // 쿠키 유효 시간 : 1시간
-		httpServletResponse.addCookie(cookie);
-
-		return jwtToken;
+		return jwtTokenProvider.generateToken(authentication);
 	}
 
 	@Override
