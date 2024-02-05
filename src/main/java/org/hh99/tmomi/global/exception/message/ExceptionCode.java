9627801753e4432d91e2aac6b0e75e8ff212883f
mum <mumum3521@naver.com>
2024-02-05 20:49:41 +0900

    Refactor : 이벤트 서비스 로직

--- src/main/java/org/hh99/tmomi/global/exception/message/ExceptionCode.java
+++ src/main/java/org/hh99/tmomi/global/exception/message/ExceptionCode.java
@@ -8,8 +8,12 @@ public enum ExceptionCode {
 	//TODO 유저,티켓,행사,공연장,좌석 에러 번호 설정?
 	// 티켓
 	NOT_EXIST_TICKET("티켓이 존재하지 않습니다."),
+
 	// 공연장
-	NOT_EXIST_STAGE("공연장이 존재하지 않습니다.");
+	NOT_EXIST_STAGE("공연장이 존재하지 않습니다."),
+
+	// 이벤트
+	NOT_EXIST_EVENT("이벤트가 존재하지 않습니다.");
 
 	private final String message;
 
