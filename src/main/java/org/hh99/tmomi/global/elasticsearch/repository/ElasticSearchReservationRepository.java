bbffa0ac8c2658bc21de045144131d70338a3234
mum <mumum3521@naver.com>
2024-02-23 04:44:13 +0900

    Fix : 엘라스틱 서치 조회 수정

--- src/main/java/org/hh99/tmomi/global/elasticsearch/repository/ElasticSearchReservationRepository.java
+++ src/main/java/org/hh99/tmomi/global/elasticsearch/repository/ElasticSearchReservationRepository.java
@@ -2,9 +2,10 @@ package org.hh99.tmomi.global.elasticsearch.repository;
 
 import java.util.List;
 
+import org.hh99.tmomi.domain.reservation.Status;
 import org.hh99.tmomi.global.elasticsearch.document.ElasticSearchReservation;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ElasticSearchReservationRepository extends ElasticsearchRepository<ElasticSearchReservation, String> {
-	List<ElasticSearchReservation> findByEventTimesId(Long eventTimeId);
+	List<ElasticSearchReservation> findAllByEventTimesIdAndStatus(Long eventTimeId, Status status);
 }
