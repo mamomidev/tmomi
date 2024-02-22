4a6cfbb4b659c162c5b582f40411afa40e8dbe93
heroq <96118954+heroq@users.noreply.github.com>
2024-02-23 04:30:06 +0900

    Feat: consumer

--- src/main/java/org/hh99/tmomi/global/elasticsearch/repository/ElasticSearchReservationRepository.java
+++ src/main/java/org/hh99/tmomi/global/elasticsearch/repository/ElasticSearchReservationRepository.java
@@ -1,9 +1,10 @@
 package org.hh99.tmomi.global.elasticsearch.repository;
 
+import java.util.List;
+
 import org.hh99.tmomi.global.elasticsearch.document.ElasticSearchReservation;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ElasticSearchReservationRepository extends ElasticsearchRepository<ElasticSearchReservation, String> {
+	List<ElasticSearchReservation> findByEventTimesId(Long eventTimeId);
 }
