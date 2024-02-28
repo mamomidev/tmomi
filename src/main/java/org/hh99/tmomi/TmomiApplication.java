7682d72ddb57226df999438a0910b5d28f4e529e
heroq <96118954+heroq@users.noreply.github.com>
2024-02-29 00:22:13 +0900

    Fix: Repositories 경로 수정

--- src/main/java/org/hh99/tmomi/TmomiApplication.java
+++ src/main/java/org/hh99/tmomi/TmomiApplication.java
@@ -2,7 +2,14 @@ package org.hh99.tmomi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
+@EnableJpaRepositories(basePackages = {"org.hh99.tmomi.domain.event", "org.hh99.tmomi.domain.stage",
+	"org.hh99.tmomi.domain.ticket", "org.hh99.tmomi.domain.user"})
+@EnableRedisRepositories(basePackages = "org.hh99.tmomi.global.redis")
+@EnableElasticsearchRepositories(basePackages = "org.hh99.tmomi.domain.reservation")
 @SpringBootApplication
 public class TmomiApplication {
 
