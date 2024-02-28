d4aabb039f4f27825abfde619fc29f7ab9d0d7d9
heroq <96118954+heroq@users.noreply.github.com>
2024-02-29 02:56:33 +0900

    Fix: redisKeyValueTemplate 삭제

--- src/main/java/org/hh99/tmomi/global/config/RedisConfig.java
+++ src/main/java/org/hh99/tmomi/global/config/RedisConfig.java
@@ -3,8 +3,6 @@ package org.hh99.tmomi.global.config;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.keyvalue.core.KeyValueAdapter;
-import org.springframework.data.keyvalue.core.KeyValueTemplate;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
@@ -26,10 +24,7 @@ public class RedisConfig {
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
 	}
-	@Bean
-	public KeyValueTemplate redisKeyValueTemplate() {
-		return new KeyValueTemplate((KeyValueAdapter) redisTemplate());
-	}
+	
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
