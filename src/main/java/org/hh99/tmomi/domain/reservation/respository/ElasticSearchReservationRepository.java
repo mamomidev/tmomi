421a6393fe7e10431298111c36c717808c1d7b25
TaeHoon0 <kimtahwn@naver.com>
2024-02-29 03:05:57 +0900

    Refactor : RedisConfig 수정

--- src/main/java/org/hh99/tmomi/domain/reservation/respository/ElasticSearchReservationRepository.java
+++ src/main/java/org/hh99/tmomi/domain/reservation/respository/ElasticSearchReservationRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.hh99.tmomi.domain.reservation.Status;
 import org.hh99.tmomi.domain.reservation.document.ElasticSearchReservation;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ElasticSearchReservationRepository extends ElasticsearchRepository<ElasticSearchReservation, String> {
 	List<ElasticSearchReservation> findAllByEventTimesIdAndStatus(Long eventTimeId, Status status);
 }
