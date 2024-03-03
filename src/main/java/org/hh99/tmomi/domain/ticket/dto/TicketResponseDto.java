7f8d1e97c9ca0027b1ad2bac9c3b8f6a504f0d75
TaeHoon0 <kimtahwn@naver.com>
2024-03-04 02:07:55 +0900

    Fix : TicketResponseDto 수정

--- src/main/java/org/hh99/tmomi/domain/ticket/dto/TicketResponseDto.java
+++ src/main/java/org/hh99/tmomi/domain/ticket/dto/TicketResponseDto.java
@@ -8,13 +8,13 @@ import lombok.Getter;
 @Getter
 public class TicketResponseDto {
 
-	private Long id;
-	private Long userId;
-	private Long eventId;
-	private Long seatId;
-	private String reservationId;
-	private Integer seatNumber;
-	private Status status;
+	private final Long id;
+	private final Long userId;
+	private final Long eventId;
+	private final Long seatId;
+	private final String reservationId;
+	private final Integer seatNumber;
+	private final Status status;
 
 	public TicketResponseDto(Ticket ticket) {
 		this.id = ticket.getId();
